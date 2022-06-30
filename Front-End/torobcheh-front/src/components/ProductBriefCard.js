e3d1be74bdf8bae4058a79a5ea542724d935d9dc
MJSahebnasi <mjsahebnasi@gmail.com>
2022-06-30 22:20:27 +0430

    tiny changes

--- Front-End/torobcheh-front/src/components/ProductBriefCard.js
+++ Front-End/torobcheh-front/src/components/ProductBriefCard.js
@@ -3,6 +3,25 @@ import React from 'react';
 
 function ProductBriefCard(props) {
 
+    if (props.noLike)
+    return (
+        <div className='ProductBriefCard'>
+            <div className='ProductBriefCard_img_div'>
+                <img src={props.img} alt={"cannot load image"} />
+            </div>
+            <div className='ProductBriefCard_title_div'>
+                نام کالا
+                {/* <Link style={{ all: "unset", cursor: "pointer" }} to={`/${props.cardData.id}`}>{props.cardData.title} </Link> */}
+            </div>
+            <div className='ProductBriefCard_price_div'>
+                {`تومان ${props.price} از`}
+            </div>
+            <div className='ProductBriefCard_shopsNum_div'>
+                {`فروشگاه ${props.shopsNum} در`}
+            </div>
+        </div>
+    );
+
     return (
         <div className='ProductBriefCard'>
             <div className='ProductBriefCard_img_div'>
@@ -19,7 +38,8 @@ function ProductBriefCard(props) {
                 {`فروشگاه ${props.shopsNum} در`}
             </div>
             <div className='ProductBriefCard_like_div'>
-                <img src={'./like_icon.jpg'} />
+                {/* <img src={'./like_icon.jpg'} /> */}
+                <button> افزودن به علاقمندی ها </button>
             </div>
         </div>
     );
