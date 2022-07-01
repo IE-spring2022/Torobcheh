06b191b2f2e250b567fe065de10f78f5fee09afb
MJSahebnasi <mjsahebnasi@gmail.com>
2022-07-02 02:55:26 +0430

    admin prof init

---
+++ Front-End/torobcheh-front/src/components/AdminProf.js
@@ -0,0 +1,24 @@
+import React from 'react';
+// import { useDispatch } from "react-redux";
+// import { login, logout } from "../redux/UserInfo";
+// import { useSelector } from "react-redux";
+import { Link } from 'react-router-dom';
+import { send_request } from '../send_request';
+
+
+function AdminProf(props) {
+
+    // const my_dispatch = useDispatch();
+    // const user_info = useSelector((state) => state.UserInfo);
+
+    return (
+        <div className="AdminProf_container">
+             {/* کالا */}
+            {/* <div>
+                افزودن زیر دسته
+            </div> */}
+        </div>
+    );
+
+}
+export default AdminProf;
