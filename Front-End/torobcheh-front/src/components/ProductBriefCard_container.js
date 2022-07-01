879a892fed39c875bf97acfad75a7abb33303a7f
MJSahebnasi <mjsahebnasi@gmail.com>
2022-07-01 11:21:53 +0430

    some minor features

--- Front-End/torobcheh-front/src/components/ProductBriefCard_container.js
+++ Front-End/torobcheh-front/src/components/ProductBriefCard_container.js
@@ -10,13 +10,29 @@ class ProductBriefCard_container extends React.Component {
         // TODO
         const cardsData = [1,2,3,4,5,6,7,8,9];
 
+        let category = 'موبایل'
+        let sub_category = 'اپل'
+
         for (var i = 0; i < cardsData.length; i++) {
             cardComponents.push(<ProductBriefCard key={i} />);
         }
 
         return (
             <div className='ProductBriefCard_container'>
-                {cardComponents}
+                <div className='sortby_div'>
+                    <select>
+                        <option value="" selected disabled>مرتب بر اساس</option>
+                        <option value="cheapest">ارزان ترین</option>
+                        <option value="most_expensive">گران ترین</option>
+                        <option value="newest">جدید ترین</option>
+                    </select>
+                    <span>
+                        {category} / {sub_category}
+                    </span>
+                </div>
+                <div className='products_container'>
+                    {cardComponents}
+                </div>
             </div>
         );
     };
