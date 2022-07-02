76e29a66180fd76817f3216e7877aceea0b5cd09
Morteza Kazemi <azetrom.kazemi2000@gmail.com>
2022-07-02 18:35:35 +0430

    Initiate getting reports for a seller

--- Back-End/route/reports.js
+++ Back-End/route/reports.js
@@ -2,6 +2,7 @@ const moment = require('moment');
 const express = require("express");
 const Product = require("../model/product");
 const User = require("../model/user");
+const Seller = require("../model/seller");
 const Report = require("../model/report")
 const Attribute = require("../model/attribute")
 const app = express();
@@ -18,6 +19,39 @@ app.post( "/api/reports", async (req, res) => {
             let report = new Report({ issuer: user, product: product, text: text }); //todo check what frontend passes for product and how to cast it to product objectId
             await report.save();
             res.status(200).json({report, message: "report successfully added"} );
+
+        } catch (err) {
+            console.log(err.message);
+            res.status(400).send("Error in Saving");
+        }
+    }
+);
+
+app.get("/api/reports/seller", async (req, res) => {
+        const { username } = req.body; //seller username
+        try {
+            let seller = await Seller.findOne({ username : username });
+            if (!seller) { return bad_request(res, "seller does not exist");}
+
+            // get all the shops of that seller -> then get all the products attached to that shop
+            // -> return all the reports of those products
+            let shops = seller.shops
+            let products = []
+            for (const shop of shops) {
+                let productsOfShop = await Product.find({ shop: shop });
+                for (const productOfShop of productsOfShop){
+                    products.push(productOfShop)
+                }
+            }
+            let reports = []
+            for (const product in products){
+                let reportsOfProd = await Report.find({product: product})
+                for (const reportOfProd of reportsOfProd){
+                    reports.push(reportOfProd)
+                }
+            }
+
+            res.status(200).json({reports} );
 
         } catch (err) {
             console.log(err.message);
