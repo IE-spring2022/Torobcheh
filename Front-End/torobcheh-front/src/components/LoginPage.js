d59bb00f988796a742657a52b261c6c3ee95d241
MJSahebnasi <mjsahebnasi@gmail.com>
2022-07-02 12:04:04 +0430

    login finally DONE

--- Front-End/torobcheh-front/src/components/LoginPage.js
+++ Front-End/torobcheh-front/src/components/LoginPage.js
@@ -1,15 +1,35 @@
 import React from 'react';
-// import { useDispatch } from "react-redux";
-// import { login, logout } from "../redux/UserInfo";
-// import { useSelector } from "react-redux";
-import { Link } from 'react-router-dom';
+import { useDispatch } from "react-redux";
+import { login, logout } from "../redux/UserInfo";
+import { useSelector } from "react-redux";
+import { Link, Navigate } from 'react-router-dom';
 import { send_request } from '../send_request';
 
+async function signup(method, url, body){
+    let res;
+    try{
+        res = await send_request(method, url, body);
+        console.log('res:',res);
+    }
+    catch(e){
+       console.log('signup oops ...');
+    }
+    if (res[0]){
+        // success
+        res = res[1];
+        return {token: res.token, user_type: res.user_type}
+    }
+    alert(res[1]);
+}
 
 function LoginPage(props) {
 
-    // const my_dispatch = useDispatch();
-    // const user_info = useSelector((state) => state.UserInfo);
+    const my_dispatch = useDispatch();
+
+    const user_info = useSelector((state) => state.UserInfo);
+    if (user_info.user_type)
+        // already loged in
+        return(<Navigate to="/" />);
 
     return (
         <div className="LoginPage_container">
@@ -18,39 +38,53 @@ function LoginPage(props) {
                     <div className='LoginPage_main_card_section_title'>
                         ثبت نام
                     </div>
-                    <div className='loginpage_form'>
-                        <input type={'text'} placeholder={'نام کاربری'} />
-                        <input type={'email'} placeholder={'ایمیل'} />
-                        <input type={'password'} placeholder={'رمز عبور'} />
-                        <input type={'number'} placeholder={'شماره موبایل'} />
-                        <select>
-                            <option value="" disabled selected> ثبت نام به عنوان </option>
-                            <option value="buyer">خریدار</option>
-                            <option value="seller">فروشنده</option>
-                        </select>
-                    </div>
-                    <div className='loginpage_button_div'>
-                        <button id='signup_button'> ثبت نام </button>
-                    </div>
+                    <form>
+                        <div className='loginpage_form'>
+                            <input id='signup_username' type={'text'} placeholder={'نام کاربری'} required />
+                            <input id='signup_email' type={'email'} placeholder={'ایمیل'} required />
+                            <input id='signup_password' type={'password'} placeholder={'رمز عبور'} required />
+                            <input id='signup_phone' type={'number'} placeholder={'شماره موبایل'} required />
+                            <select id='signup_type_select' required>
+                                <option value="" disabled selected> ثبت نام به عنوان </option>
+                                <option value="user">خریدار</option>
+                                <option value="seller">فروشنده</option>
+                            </select>
+                        </div>
+                        <div className='loginpage_button_div'>
+                            <button type='button'
+                                onClick={async() => my_dispatch(login(await signup("POST", 'auth/signup/'+document.getElementById("signup_type_select").value
+                                    ,JSON.stringify({
+                                        username: document.getElementById("signup_username").value
+                                        ,email: document.getElementById("signup_email").value
+                                        ,password:document.getElementById("signup_password").value
+                                        ,phone:document.getElementById("signup_phone").value
+                                    }))))}
+                                        
+                                id='signup_button'> ثبت نام 
+                            </button>
+                        </div>
+                    </form>
                 </div>
 
                 <div id='login_div' className='LoginPage_main_card_section'>
                     <div className='LoginPage_main_card_section_title'>
                         ورود
                     </div>
-                    <div className='loginpage_form'>
-                        <input type={'text'} placeholder={'نام کاربری'} />
-                        <input type={'password'} placeholder={'رمز عبور'} />
-                    </div>
-                    <div className='loginpage_button_div'>
-                        <button 
-                            // onClick={() => { my_dispatch(login({token: "abc", user_type: "seller"})); }} 
-                            onClick={() => send_request("POST", 'auth/login/seller', JSON.stringify({username:'user1',password:'pass1'}))}
-                            // onClick={() => send_request("GET", 'attributes')}
-                            id='login_button'> 
-                            ورود 
-                        </button>
-                    </div>
+                    <form>
+                        <div className='loginpage_form'>
+                            <input type={'text'} placeholder={'نام کاربری'} required />
+                            <input type={'password'} placeholder={'رمز عبور'} required />
+                        </div>
+                        <div className='loginpage_button_div'>
+                            <button 
+                                // onClick={() => { my_dispatch(login({token: "abc", user_type: "seller"})); }} 
+                                onClick={() => send_request("POST", 'auth/login/seller', JSON.stringify({username:'user1',password:'pass1'}))}
+                                // onClick={() => send_request("GET", 'attributes')}
+                                id='login_button'> 
+                                ورود 
+                            </button>
+                        </div>
+                    </form>
                 </div>
             </div>
 
--- Front-End/torobcheh-front/src/send_request.js
+++ Front-End/torobcheh-front/src/send_request.js
@@ -1,24 +1,32 @@
-export function send_request(method, end_point, body=null) {
-    let xhttp = new XMLHttpRequest();
-    xhttp.addEventListener("readystatechange", () => {
-        if (xhttp.readyState === 4) {
-            if (xhttp.status === 200) {
-                // request successful
-                let data = JSON.parse(xhttp.responseText);
-                console.log('data:', data);
-                return data;
-            } else {
-                // request failed
-                console.log("error getting data:")
-                console.log(xhttp.responseText)
+export function send_request(method, end_point, body = null) {
+    console.log('-----send req-----');
+    console.log(end_point);
+    console.log(body);
+    console.log('------------------');
+
+    return new Promise((resolve, reject) => {
+        let xhttp = new XMLHttpRequest();
+        xhttp.addEventListener("readystatechange", () => {
+            if (xhttp.readyState === 4) {
+                if (xhttp.status === 200) {
+                    // request successful
+                    let data = JSON.parse(xhttp.responseText);
+                    resolve([true, data]);
+                } else {
+                    // request failed
+                    // let data = JSON.parse(xhttp.responseText);
+                    let data = xhttp.responseText;
+                    console.log("error getting data:");
+                    console.log(data);
+                    resolve([false, data]);
+                }
             }
-        }
-    });
-    console.log('--');
-    xhttp.open(method, "http://localhost:3000/api/" + end_point);
-    xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    if (body)
-        xhttp.send(body);
-    else
-        xhttp.send();
+        });
+        xhttp.open(method, "http://localhost:3000/api/" + end_point);
+        xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+        if (body)
+            xhttp.send(body);
+        else
+            xhttp.send();
+    })
 }
