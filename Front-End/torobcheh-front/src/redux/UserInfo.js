51a713ee8a6790be281c0a5b7b1e630cad360690
MJSahebnasi <mjsahebnasi@gmail.com>
2022-07-03 09:05:22 +0430

    query added

--- Front-End/torobcheh-front/src/redux/UserInfo.js
+++ Front-End/torobcheh-front/src/redux/UserInfo.js
@@ -2,20 +2,34 @@ import { createSlice } from "@reduxjs/toolkit";
 
 export const UserInfo = createSlice({
     name: "UserInfo",
-    initialState: { user_id: null, token: null, user_type: null },
+    initialState: { user_id: null, token: null, user_type: null, search_query: null },
     reducers: {
         login: (state = {}, action) => {
             return {
                 user_id: action.payload.user_id,
                 token : action.payload.token,
-                user_type : action.payload.user_type
+                user_type : action.payload.user_type,
+                search_query: null
             }
         },
         logout: (state = {}) => {
             return {
                 user_id: null,
                 token : null,
-                user_type : null
+                user_type : null,
+                search_query: null
+            }
+        },
+        set_query: (state = {}, action) => {
+            return {
+                ... state,
+                search_query: action.payload.query
+            }
+        },
+        reset_query: (state = {}) => {
+            return {
+                ... state,
+                search_query: null
             }
         }
     },
