4a3c1981d25645a257b42857e38b5e9bedf38196
MJSahebnasi <mjsahebnasi@gmail.com>
2022-07-03 15:33:58 +0430

    logout button

--- Front-End/torobcheh-front/src/components/ProfilePage.js
+++ Front-End/torobcheh-front/src/components/ProfilePage.js
@@ -1,21 +1,36 @@
 import React from 'react';
-// import { useDispatch } from "react-redux";
-// import { login, logout } from "../redux/UserInfo";
-import { useSelector } from "react-redux";
-// import { Link } from 'react-router-dom';
-// import { send_request } from '../send_request';
+import { logout } from "../redux/UserInfo";
+import { useSelector, useDispatch } from "react-redux";
+import { Navigate } from 'react-router-dom';
 import SellerProf from './SellerProf';
 import AdminProf from './AdminProf';
 
 function ProfilePage(props) {
 
     const user_info = useSelector((state) => state.UserInfo);
+    const my_dispatch = useDispatch();
+
+    if(!user_info.user_type)
+        return <Navigate to={'/'} />
+
+    let main_body;
 
     if(user_info.user_type === "user")
-        return 
+        main_body = null; 
     else if((user_info.user_type === "seller"))
-        return <SellerProf />
+        main_body = <SellerProf />
     else if(user_info.user_type === "admin")
-        return <AdminProf />
+        main_body = <AdminProf />
+
+    return (
+        <div id="logout_div">
+            <button type='button'
+                onClick={() => my_dispatch(logout())}>
+                خروج از حساب
+            </button>
+            <br></br>
+            {main_body}
+        </div>
+    );
 }
 export default ProfilePage;
